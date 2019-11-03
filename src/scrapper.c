a6718450726a48449f49f1d1e4d91ba58b74d113
Antonin <antonin.texier@gmail.com>
2019-11-03 18:42:56 +0100

    another test

--- src/scrapper.c
+++ src/scrapper.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <stdlib.h>
-#include <curl/curl.h>
+
 #include <string.h>
 #include <unistd.h>
 #include <headers/structures.h>
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
     tasksList = getTasksListFromConfigFile(argv[1]);
-    //displayTasksList(tasksList);
+    displayTasksList(tasksList);
     test(tasksList);
 
     // freeTaskFromList(tasksList);
@@ -25,34 +25,3 @@ int main(int argc, char** argv) {
     
     return EXIT_SUCCESS;
 }
-
-
-
-
-
-
-
-
-    // EXAMPLE OF CURL REQUEST
-    //
-    // CURLcode res;
-    // CURL* curl = NULL;
-    // FILE *fp = fopen("tests_curl/test.html", "wb");
-    // if(fp == NULL) return 1;
-    // curl = curl_easy_init();
-    // if(curl != NULL) {
-    //     curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.fr/");
-    //     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data); 
-	//     curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-    //     res = curl_easy_perform(curl);
-    //     curl_easy_cleanup(curl);
-    //     printf("\nnew file created\n");
-    // }
-    // fclose(fp);
-    // freeTask(&actionsList);
-    // while (tasksList->firstTask != NULL)
-    // {
-    //     freeTaskFromList(tasksList);
-    // }
-    // displayActionsList(actionsList);
-    // displayTasksList(tasksList);
--- src/sniff.c
+++ src/sniff.c
@@ -1,7 +1,20 @@
 #include "headers/sniff.h"
 #include "headers/structures.h"
-
+#include <curl/curl.h>
 void test(TaskList* tasklist) {
-    displayTasksList(tasklist);
-    printf("\ntototo\n");
+        // EXAMPLE OF CURL REQUEST
+    //
+    // CURLcode res;
+    CURL* curl = NULL;
+    FILE *fp = fopen("tests_curl/test.html", "wb");
+    if(fp == NULL) return 1;
+    curl = curl_easy_init();
+    if(curl != NULL) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.fr/");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data); 
+	    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        printf("\nnew file created\n");
+    }
 }
